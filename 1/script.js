e26212cc931ac19c421fe82141e17b55b5f3ea9e
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-10-30 13:57:39 -0700

    assemssent 1 complete

---
+++ 1/script.js
@@ -0,0 +1,23 @@
+// const fileUpload = document.getElementById('file-upload')
+// const userImage = document.getElementById('user-image')
+// const displayMessage = document.getElementById('display-message')
+// const inputMessage = document.getElementById('input-message')
+// const form = document.getElementById('form')
+//
+// form.onsubmit = function(e) {
+//   e.preventDefault()
+// }
+// 
+// inputMessage.oninput = function(e) {
+//   console.log(e.target.value)
+//   displayMessage.innerHTML = e.target.value
+// }
+//
+// fileUpload.onchange = function(e) {
+//   console.log(e.target.files)
+//   const fr = new FileReader()
+//   fr.onload = function(e) {
+//     userImage.src = e.target.result
+//   }
+//   fr.readAsDataURL(e.target.files[0])
+// }
